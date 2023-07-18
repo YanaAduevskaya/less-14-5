c9a5a7e1c7f124051d83db840a909799827295f1
YanaAduevskaya <kambel91@mail.ru>
2023-07-19 00:51:08 +0300

    add projects, testimonialv

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 //     var elems = document.querySelectorAll('.sidenav');
 //     var instances = M.Sidenav.init(elems);
 //   });
-// document.addEventListener('DOMContentLoaded', function() {
-//   var gallery = document.querySelectorAll('.materialboxed');
-//   var instances_gallery = M.Materialbox.init(gallery);
-// });
+document.addEventListener('DOMContentLoaded', function() {
+    var gallery = document.querySelectorAll('.materialboxed');
+    var instances_gallery = M.Materialbox.init(gallery);
+  });
